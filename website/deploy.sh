72c6779eb00046842b97e3d0027f6d97d675941e
Colin <colin.diesh@gmail.com>
2018-08-08 01:48:22 -0400

    Avoid deleted files from release build

--- website/deploy.sh
+++ website/deploy.sh
@@ -2,5 +2,6 @@
 cd ${0%/*}
 yarn
 yarn build
-ls ../
-scp -i ../deploy.pem -o StrictHostKeyChecking=no -r build cdiesh@jbrowse.org:~/
+openssl aes-256-cbc -K $encrypted_a33f2fb5219d_key -iv $encrypted_a33f2fb5219d_iv -in deploy.pem.enc -out deploy.pem -d
+chmod 600 deploy.pem
+scp -i deploy.pem -o StrictHostKeyChecking=no -r build cdiesh@jbrowse.org:~/
