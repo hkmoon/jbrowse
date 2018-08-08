60944b02ffde37a69928f8660d724f53ff755195
Colin <colin.diesh@gmail.com>
2018-08-08 00:09:11 -0400

    The yarn build cleans out system

--- website/deploy.sh
+++ website/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd ${0%/*}
-echo `pwd`
-ls
 yarn
 yarn build
-scp -i deploy.pem -o StrictHostKeyChecking=no -r build cdiesh@jbrowse.org:~/
+scp -i ../deploy.pem -o StrictHostKeyChecking=no -r build cdiesh@jbrowse.org:~/
